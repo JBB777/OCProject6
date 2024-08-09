cb3e2655fca43e9828c4032efd0811aba8ae02d4
JBB <jbaptistebesancon@gmail.com>
2024-08-09 12:07:36 +0200

    Gestion login + messages erreurs si le duo Mail/Password n'est pas le bon + redirection vers index si login OK

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,12 +1,9 @@
 
-function mailVerification(mail) {
-    var regexMail = new RegExp('^[a-z0-9-.]+@[a-z0-9-.]+\.[a-z]+$');
-    return regexMail.test(mail);
-}; 
-
 
 const formLogin = document.getElementById("formLogin");
-formLogin.addEventListener('submit', function(log){
+formLogin.addEventListener('submit', async function(log){
+    log.preventDefault();
+
     let userMail = document.getElementById("email");
     let userPassword = document.getElementById("password");
     let error = document.getElementById("error");
@@ -14,28 +11,32 @@ formLogin.addEventListener('submit', function(log){
 
     if ((userMail.value == "") || (userPassword.value == "")){
         error.innerHTML = "L'un des champs est vide !!!";
-        log.preventDefault();
     }
-    else {
-        if (mailVerification(userMail.value) != true) {
-            error.innerHTML = "L'adresse mail n'est pas valide.";
-            log.preventDefault();
+    else { 
+        const userAuth = {
+            email : userMail.value,
+            password : userPassword.value
+        }
+        const chargeUserAuth = JSON.stringify(userAuth);
+
+        const fetchResponse = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUserAuth
+        });
+        
+        if (fetchResponse.status == 200) {
+            const responseToken = await fetchResponse.json();
+            window.localStorage.setItem("userToken", responseToken.token);
+            window.location.href = "index.html";
         }
         else {
-            /*const userAuth = {
-                email : userMail.value,
-                password : userPassword.value
+            if (fetchResponse.status == 401) {
+                error.innerHTML = "Mot de passe incorrect";
             }
-            const chargeUserAuth = JSON.stringify(userAuth);
-
-            fetch("http://localhost:5678/api/users/login", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: chargeUserAuth
-            });*/
-            
-            console.log("Log possible");
-            log.preventDefault();
+            if (fetchResponse.status == 404) {
+                error.innerHTML = "Utilisateur non trouvé";
+            }
         }
     }
 });
